3acb6b608b8b60817de683e015016065c10e3201
Golub Anton <antongolub1992@gmail.com>
2022-10-26 14:47:01 +0300

    markup finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom"
+import {Home} from "./Pages/Home";
+import {About} from "./Pages/About";
+import {Navbar} from "./Components/Navbar";
+import {Alert} from "./Components/Alert";
+
 function App() {
   return (
-    <div className="container">
-        <h1>Test</h1>
-    </div>
+    <BrowserRouter>
+        <Navbar/>
+        <div className="container pt-4">
+            <Alert />
+            <Switch>
+                <Route path={'/'} exact component={Home}/>
+                <Route path={'/about'} component={About}/>
+            </Switch>
+        </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const About = () => (
+        <div className="jumbotron">
+            <div className="container">
+                <h1 className="display-4">React App</h1>
+                <p>Version build <strong>1.0</strong></p>
+            </div>
+        </div>
+    );
