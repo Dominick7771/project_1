8ad891b1ad49445960ea340a723aa87027c73d81
Golub Anton <antongolub1992@gmail.com>
2022-10-26 15:28:11 +0300

    added some logic

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import {Home} from "./Pages/Home";
 import {About} from "./Pages/About";
 import {Navbar} from "./Components/Navbar";
 import {Alert} from "./Components/Alert";
+import {AlertState} from "./Context/alert/AlertState";
 
 function App() {
-  return (
-    <BrowserRouter>
-        <Navbar/>
-        <div className="container pt-4">
-            <Alert />
-            <Switch>
-                <Route path={'/'} exact component={Home}/>
-                <Route path={'/about'} component={About}/>
-            </Switch>
-        </div>
-    </BrowserRouter>
-  );
+    return (
+        <AlertState>
+            <BrowserRouter>
+                <Navbar/>
+                <div className="container pt-4">
+                    <Alert/>
+                    <Switch>
+                        <Route path={'/'} exact component={Home}/>
+                        <Route path={'/about'} component={About}/>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        </AlertState>
+    );
 }
 
 export default App;
